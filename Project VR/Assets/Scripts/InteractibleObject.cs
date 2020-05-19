74db83f42d428e74889ba228a011f9d1998136e4
lazlo barragan <lazlo.barragan@gmail.com>
2020-05-20 00:28:40 +0200

    cleanup

--- Project VR/Assets/Scripts/InteractibleObject.cs
+++ Project VR/Assets/Scripts/InteractibleObject.cs
@@ -5,25 +5,9 @@ public class InteractibleObject : MonoBehaviour
 {
     public Piece piece;
     public TextMeshPro worldTMProName;
-    public GameObject traceLineToTextParent;
-
-    bool selected = false;
 
     private void Start()
     {
         if(worldTMProName!=null) worldTMProName.text = piece.name;
-    }
-
-    public void Select()
-    {
-        UIManager.instance.UpdateContent(piece);
-        if(traceLineToTextParent!=null) traceLineToTextParent.SetActive(true);
-        selected = true;
-    }
-    public void UnSelect()
-    {
-        UIManager.instance.UpdateContent();
-        if (traceLineToTextParent != null)  traceLineToTextParent.SetActive(false);
-        selected = false;
     }
 }
